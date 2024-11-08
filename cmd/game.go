3d0e0dc16e416ee88168103ded7abdd1f11c2d04
3Marz <omar490v@gmail.com>
2024-11-08 14:48:12 +0300

    added checking for winners in server

--- cmd/game.go
+++ cmd/game.go
@@ -34,6 +34,38 @@ func newGame() *Game {
 	}
 }
 
+func (h *Game) checkWinner() (result string) {
+	result = ""
+	winPoses := [][][]int{
+		{{0,0},{0,1},{0,2}},
+		{{1,0},{1,1},{1,2}},
+		{{2,0},{2,1},{2,2}},
+
+		{{0,0},{1,0},{2,0}},
+		{{0,1},{1,1},{2,1}},
+		{{0,2},{1,2},{2,2}},
+
+		{{0,0},{1,1},{2,2}},
+		{{2,0},{1,1},{0,2}},
+	}
+
+	for i := 0; i < len(winPoses); i++ {
+		pos1 := winPoses[i][0]
+		pos2 := winPoses[i][1]
+		pos3 := winPoses[i][2]
+
+		mark1 := h.board[pos1[0]][pos1[1]]
+		mark2 := h.board[pos2[0]][pos2[1]]
+		mark3 := h.board[pos3[0]][pos3[1]]
+
+		if (mark1 == mark2 && mark1 == mark3 && mark2 == mark3) {
+			result = mark1
+		}
+	}
+
+	return
+}
+
 func (h *Game) run() {
 	for {
 		select {
@@ -54,11 +86,14 @@ func (h *Game) run() {
 				}
 			}
 		case board := <-h.boardChannal:
+			winner := h.checkWinner()
 			for player := range h.players {
 				player.board <- board
 				if(player.mark != h.turn) {
-					fmt.Println(player.mark)
 					player.message <- "Your Turn"
+				}
+				if(winner == player.mark) {
+					player.message <- "You Win"
 				}
 			}
 			if h.turn == "X" { h.turn = "O" } else { h.turn = "X" }
